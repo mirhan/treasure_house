0dbedf4597f0b30a561c18ea9e9e40b24f2d975a
mirhan <handachang@gmail.com>
2016-04-01 10:34:17 +0800

    添加 Main, Advance, Show

--- learn_django/mysite/west/admin.py
+++ learn_django/mysite/west/admin.py
@@ -3,7 +3,15 @@ from west.models import Character,Contact,Tag
 
 # Register your models here.
 class ContactAdmin(admin.ModelAdmin):
-    fields = ('name', 'email')
+    fieldsets = (
+        ['Main',{
+            'fields':('name','email'),
+        }],
+        ['Advance',{
+            'classes': ('collapse',), # CSS
+            'fields': ('age',),
+        }]
+    )
 
 admin.site.register(Contact, ContactAdmin)
 admin.site.register([Character, Tag])
