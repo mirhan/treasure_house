cf43aa9f9275426c4c0a977209e4115497fcebd7
mirhan <handachang@gmail.com>
2016-04-01 14:14:31 +0800

    login 使用 forms 对象。 简化代码和模板

--- learn_django/mysite/users/views.py
+++ learn_django/mysite/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.core.context_processors import csrf
 from django.contrib.auth import *
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def user_login(request):
@@ -9,15 +10,15 @@ def user_login(request):
     '''
     print '0'
     if request.POST:
-        username = password = ''
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user     = authenticate(username=username, password=password)
-        if user is not None and user.is_active:
-                print '1'
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = authenticate(username = request.POST['username'], password=request.POST['password'])
+            if user is not None and user.is_active:
                 login(request, user)
                 return redirect('/')
+
+    form = AuthenticationForm()
     ctx = {}
     ctx.update(csrf(request))
-    print '2'
+    ctx['form'] = form
     return render(request, 'login.html',ctx)
