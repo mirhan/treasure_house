9637b315578a1eda8f5c1ffec53370fcdc684980
mirhan <handachang@gmail.com>
2016-04-01 10:31:56 +0800

    只显示 Contact 的部分条目

--- learn_django/mysite/west/admin.py
+++ learn_django/mysite/west/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from west.models import Character,Contact,Tag
 
 # Register your models here.
-admin.site.register([Character, Contact, Tag])
+class ContactAdmin(admin.ModelAdmin):
+    fields = ('name', 'email')
+
+admin.site.register(Contact, ContactAdmin)
+admin.site.register([Character, Tag])
+
